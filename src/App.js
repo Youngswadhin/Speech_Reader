d059b61dbcee88fc3b62b8d273eaefcf44d0e010
Youngswadhin <swadhinsocialmedia@gmail.com>
2024-06-26 11:00:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import logo from "./note.png";
+import "./App.css";
 
 function App() {
+  const [showTextBox, setShowTextBox] = useState(false);
+  const inputRef = useRef(null);
+  const textBoxRef = useRef(null);
+
+  function speak() {
+    const text = inputRef.current.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+  }
+
+  function handleButtonClick() {
+    setShowTextBox(true);
+  }
+
+  function handleClickOutside(event) {
+    if (textBoxRef.current && !textBoxRef.current.contains(event.target)) {
+      setShowTextBox(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div class="container">
-       <nav>
-        <img src="note.png" class="logo" height="40px" width="40px" alt="note-pic">
-          <h1 class="head" style="color: white;"></h1>
-       
-          <button class="button">Create Notes</button>
-       </nav>
-       <h1 class="heading">Notes</h1>
-       <div class="notes-container">
-          <!-- <p contenteditable="true" class="input-box"> 
-           <img class="red" src="https://imgs.search.brave.com/A8yvL5i5o8kRW8qeseBGyG_BH8MlyiHOzOw-McOEP0c/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMTMvUmVk/LUNpcmNsZS5wbmc" alt="">
-          </p> -->
-       </div>
+    <div className="container">
+      <nav>
+        <img
+          src={logo}
+          className="logo"
+          height="40px"
+          width="40px"
+          alt="note-pic"
+        />
+        <button className="button" onClick={handleButtonClick}>
+          Read Speech
+        </button>
+      </nav>
+      <h1 className={`heading ${showTextBox ? "blur" : ""}`}>Notes</h1>
+      {showTextBox && (
+        <div className="textBox" ref={textBoxRef}>
+          <textarea id="input" ref={inputRef} defaultValue="Notes"></textarea>
+          <div className="spkbtn">
+            <button className="spk_btn" onClick={speak}>Speak</button>
+          </div>
+        </div>
+      )}
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
-    <script src="script.js"></script>
   );
 }
 
